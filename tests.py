ff1ddf14246263bcb23e3bfef17fd27c9ddd1619
fruitsamurai97 <bendaou.med.gin@gmail.com>
2024-04-04 00:23:47 +0200

    pytests update
    due to chaning azure key calling structure

--- tests.py
+++ tests.py
@@ -1,31 +1,40 @@
-
-import streamlit as st
+import pytest
 from functions import load_data, load_model, load_explainer
 from fct_process import extraction, extract_bounds
-import pytest
 
-def test_data():
-    assert not load_data().empty
+# Fixture to load data once and use it in multiple tests
+@pytest.fixture(scope="module")
+def loaded_data():
+    return load_data()
 
-df=load_data()
+def test_data(loaded_data):
+    assert not loaded_data.empty
+
 def test_model():
     assert load_model()
+
 def test_explainer():
     assert load_explainer()
-def test_client_overview():
-    cols=["SK_ID_CURR","IF_0_CREDIT_IS_OKAY","PAYBACK_PROBA","CODE_GENDER", "AMT_INCOME_TOTAL", "AMT_CREDIT", "AMT_ANNUITY", "AMT_GOODS_PRICE", "DAYS_BIRTH"]
-    assert set(cols).issubset(set(df.columns))
 
-def test_proba():
-    list_IDS = df["SK_ID_CURR"].unique().tolist()
-    condition = df['SK_ID_CURR'] == list_IDS[0]
-    elt = df[condition]["PAYBACK_PROBA"].tolist()[0] 
-    assert elt >= 0 
-    assert elt<=1 
+def test_client_overview(loaded_data):
+    cols = [
+        "SK_ID_CURR",
+        "IF_0_CREDIT_IS_OKAY",
+        "PAYBACK_PROBA",
+        "CODE_GENDER",
+        "AMT_INCOME_TOTAL",
+        "AMT_CREDIT",
+        "AMT_ANNUITY",
+        "AMT_GOODS_PRICE",
+        "DAYS_BIRTH",
+    ]
+    assert set(cols).issubset(set(loaded_data.columns))
 
-def test_extract_bonds():
-    assert all(item is not None and item!=""  for item in  extract_bounds(" 0< TEST_LOL <= 1 "))
+def test_proba(loaded_data):
+    list_IDS = loaded_data["SK_ID_CURR"].unique().tolist()
+    condition = loaded_data['SK_ID_CURR'] == list_IDS[0]
+    elt = loaded_data[condition]["PAYBACK_PROBA"].tolist()[0] 
+    assert 0 <= elt <= 1
 
-
-
-
+def test_extract_bounds():
+    assert all(item is not None and item != "" for item in extract_bounds(" 0< TEST_extract_bounds <= 1 "))
